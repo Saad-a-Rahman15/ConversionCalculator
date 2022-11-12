8227bdf931607c6073ed52e592e196ccc87156b9
Saad-A-Rahman15 <saad.a.rahman15@gmail.com>
2022-11-12 20:06:42 +0000

    Check out the temperature!

---
+++ celcius-farenheit_converter.py
@@ -0,0 +1,37 @@
+print('Type one of of the numbers below')
+print('1 = celcius converted to farenheit')
+print('2 = farenheit converted to celcius')
+choice = input('    1    2    \n')
+
+
+if(choice == '1'):
+        celcius = input('Enter an amount of degrees in celcius. \n')
+        celcius = float(celcius)
+
+        farenheit = ( celcius * 1.8 + 32 )
+
+        if(celcius == '0' ):
+            print('0.00 degrees celcius')
+        else:
+            print('The amount of degrees celcius that would equal that many degrees farenheit is:')
+        celcius_value = celcius - 32 * 0.5556
+        celcius_value = round(celcius_value, 2)
+        print(str(celcius_value) + ' degrees farenheit')
+        print('Thank you for choosing this calculator!')
+elif(choice == '2'):
+        farenheit2 = input('Enter an amount of degrees in farenheit. \n')
+        farenheit2 = float(farenheit2)
+
+        farenheit2 = ( farenheit2 - 32 * 0.5556 )
+
+        if(farenheit2 == '0' ):
+            print('0.00 degrees celcius')
+        else:
+            print('The amount of degrees farenheit that would equal that many degrees celcius is:')
+            
+        farenheit2_value = farenheit2 * 1.8 + 32
+        farenheit2_value = round(farenheit2_value, 2)
+        print(str(farenheit2_value) + ' degrees')
+        print('Thank you for choosing this calculator!')
+else:
+    print('That cannot be done.')
