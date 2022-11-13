6aea7a3766be9836a50ead29599a2c392c95c17f
Saad-A-Rahman15 <saad.a.rahman15@gmail.com>
2022-11-13 12:28:45 +0000

    This is my first commit

--- celcius-farenheit_converter.py
+++ celcius-farenheit_converter.py
@@ -14,9 +14,8 @@ if(choice == '1'):
             print('0.00 degrees farenheit')
         else:
             print('The amount of degrees farenheit that would equal that many degrees celcius is:')
-        farenheit_value = celcius - 32 * 0.5556
-        farenheit_value = round(farenheit_value, 2)
-        print(str(farenheit_value) + ' degrees farenheit')
+        farenheit = round(farenheit, 2)
+        print(str(farenheit) + ' degrees farenheit')
         print('Thank you for choosing this calculator!')
 elif(choice == '2'):
         farenheit2 = input('Enter an amount of degrees in farenheit. \n')
@@ -29,9 +28,8 @@ elif(choice == '2'):
         else:
             print('The amount of degrees celcius that would equal that many degrees farenheit is:')
             
-        celcius2_value = farenheit2 * 1.8 + 32
-        farenheit2_value = round(celcius2_value, 2)
-        print(str(celcius2_value) + ' degrees celcius')
+        celcius2 = round(celcius2, 2)
+        print(str(celcius2) + ' degrees celcius')
         print('Thank you for choosing this calculator!')
 else:
     print('That cannot be done.')
